4f90556b23446bae5bbe236702771d0c01fad0b7
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2024-04-11 18:19:18 -0500

    List paginated comments from features

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,29 @@
 module Api
   class CommentsController < ApplicationController
+
+    MAX_PER_PAGE = 100
+
+    def index
+
+      # Validations
+      return render(validate_per_page_param) if validate_per_page_param
+
+      comments = Comment.where(feature_id: params[:feature_id])
+
+      total_count = comments.count
+      comments = comments.paginate(page: params[:page], per_page: params[:per_page])
+
+      render json: {
+        data: comments,
+        pagination: {
+          current_page: comments.current_page,
+          total: total_count,
+          per_page: comments.per_page
+        }
+      }
+
+    end
+
     def create
       comment = Comment.new(comment_params)
 
@@ -14,6 +38,17 @@ module Api
 
     def comment_params
       params.permit(:feature_id, :body)
+    end
+
+    def validate_per_page_param
+
+      per_page = params[:per_page]&.to_i
+
+      if per_page.present? && (per_page < 1 || per_page > MAX_PER_PAGE)
+        return { json: { error: "per_page param must be between: 1 and #{MAX_PER_PAGE}" }, status: :bad_request }
+      end
+
+      return false
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     resources :features, only: [:index, :show] 
-    resources :comments, only: [:create]
+    resources :comments, only: [:index, :create]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
